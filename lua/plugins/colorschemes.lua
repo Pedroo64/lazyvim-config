2baf6d1e448a23e85eaa0359660d699c69679428
Pedro Carvalho Mendes <pedrocarvalho-12@hotmail.com>
2025-11-06 15:44:54 +0100

    Add catppuccin theme

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      background = {
+        light = "latte",
+        dark = "macchiato",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  }
+}
+
