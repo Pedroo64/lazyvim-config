ba0b894f0bc4f1de7e5c1d9d57fb8d37236bb728
Pedro Carvalho Mendes <pedrocarvalho-12@hotmail.com>
2025-11-06 15:47:34 +0100

    Update configuration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.autoformat = false
+
+vim.opt.list = true
+vim.opt.listchars = {
+  tab      = "▸ ",  -- visually show tabs
+  trail    = "·",   -- show trailing spaces as dots
+  extends  = "→",   -- show when text extends past screen
+  precedes = "←",   -- show when text precedes screen
+  nbsp     = "␣",   -- show non-breaking spaces
+}
+
---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  keys = {
+    { "<leader>gn", function () require("neogit").open() end, desc = "Neogit" }
+  }
+}
+
