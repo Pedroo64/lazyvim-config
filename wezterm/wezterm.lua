7e4406a4dba95b4ce7271011e5ff7e64704bea85
Pedro Carvalho Mendes <pedrocarvalho-12@hotmail.com>
2025-11-06 15:45:44 +0100

    Add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,41 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+-- config.window_decorations = "RESIZE"
+config.window_padding = {
+  left   = 0,
+  right  = 0,
+  top    = 0,
+  bottom = 0,
+}
+
+config.audible_bell = 'Disabled'
+
+config.visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 150,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 150,
+}
+config.colors = {
+  visual_bell = '#202020',
+}
+
+config.hide_tab_bar_if_only_one_tab = true
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 12
+config.color_scheme = 'AdventureTime'
+config.font = wezterm.font('Maple Mono Normal NL NF')
+
+-- Finally, return the configuration to wezterm:
+return config
+
