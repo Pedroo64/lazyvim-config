6161376613f974232f9ebaab9c320d4f1b7d7e35
Pedro Carvalho Mendes <pedrocarvalho-12@hotmail.com>
2025-11-07 16:22:30 +0100

    Add extras

---
+++ lua/config/extras.lua
@@ -0,0 +1,10 @@
+return {
+  { import = "lazyvim.plugins.extras.coding.blink" },
+  { import = "lazyvim.plugins.extras.coding.luasnip" },
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.editor.illuminate" },
+  { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lsp.none-ls" },
+}
