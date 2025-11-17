e8fb9ac8479d487f452c2862d87c6de5c9936064
Pedro Carvalho Mendes <pedrocarvalho-12@hotmail.com>
2025-11-17 22:11:22 +0100

    Add mini plugins and vscode plugin

--- lua/config/extras.lua
+++ lua/config/extras.lua
@@ -3,8 +3,11 @@ return {
   { import = "lazyvim.plugins.extras.coding.luasnip" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
   { import = "lazyvim.plugins.extras.editor.illuminate" },
+  { import = "lazyvim.plugins.extras.editor.mini-diff" },
+  { import = "lazyvim.plugins.extras.editor.mini-move" },
   { import = "lazyvim.plugins.extras.lang.clangd" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lsp.none-ls" },
+  { import = "lazyvim.plugins.extras.vscode" },
 }
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,16 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "move line down", silent = true }) -- move line down(n)
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "move line up", silent = true }) -- move line up(n)
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "move line down", silent = true } ) -- move line down(v)
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "move line up", silent = true } ) -- move line up(v)
-
-vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==", { desc = "move line down", silent = true }) -- move line down(n)
-vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==", { desc = "move line up", silent = true }) -- move line up(n)
-vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "move line down", silent = true } ) -- move line down(v)
-vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv", { desc = "move line up", silent = true } ) -- move line up(v)
-
 -- Terminal
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {})
 
