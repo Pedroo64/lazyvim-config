9536e9f9c752d1fbbc25688c08d749005feaf3f9
Pedro Carvalho Mendes <pedrocarvalho-12@hotmail.com>
2025-11-17 22:12:05 +0100

    Update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,28 +1,31 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.enable_wayland = false
+
 -- This is where you actually apply your config choices.
 -- config.window_decorations = "RESIZE"
 config.window_padding = {
-  left   = 0,
-  right  = 0,
-  top    = 0,
+  left = 0,
+  right = 0,
+  top = 0,
   bottom = 0,
 }
 
-config.audible_bell = 'Disabled'
+config.audible_bell = "Disabled"
 
 config.visual_bell = {
-  fade_in_function = 'EaseIn',
+  fade_in_function = "EaseIn",
   fade_in_duration_ms = 150,
-  fade_out_function = 'EaseOut',
+  fade_out_function = "EaseOut",
   fade_out_duration_ms = 150,
 }
 config.colors = {
-  visual_bell = '#202020',
+  visual_bell = "#202020",
 }
 
 config.hide_tab_bar_if_only_one_tab = true
@@ -33,9 +36,14 @@ config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
 config.font_size = 12
-config.color_scheme = 'AdventureTime'
-config.font = wezterm.font('Maple Mono Normal NL NF')
+
+if wezterm.gui.get_appearance():find("Dark") then
+  config.color_scheme = "Catppuccin Macchiato"
+else
+  config.color_scheme = "Catppuccin Latte"
+end
+
+config.font = wezterm.font("Maple Mono Normal NL NF")
 
 -- Finally, return the configuration to wezterm:
 return config
-
