af29a3ea1386f236da101c9661cd59a5c9b08b99
Pedro Carvalho Mendes <pedrocarvalho-12@hotmail.com>
2025-12-01 17:58:41 +0100

    Add paste with replacing keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Paste without replacing
+vim.keymap.set("x", "P", "\"_dP")
+
 -- Terminal
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {})
 
