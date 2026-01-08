b3e57a01a4d21ae56d9047ebddd7963d9732d779
Pedro Carvalho Mendes <pedrocarvalho-12@hotmail.com>
2026-01-08 09:58:50 +0100

    [Snacks]: Disable terminal auto insert

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,5 +2,11 @@ return {
   "folke/snacks.nvim",
   keys = {
     { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+  },
+  opts = {
+    terminal = {
+      start_insert = false,
+      auto_insert = false,
+    },
   }
 }
