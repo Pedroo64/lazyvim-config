f18677e2a774ed03e35a49e9f4c72fdcd0245a41
Pedro Carvalho Mendes <pedrocarvalho-12@hotmail.com>
2026-01-24 18:43:49 +0100

    Add oil file explorer

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,22 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = false,
+  -- stylua: ignore start
+  keys = {
+    { "<leader>fo", function () require("oil").open() end, desc = "Open Oil explorer (Directory or Current file)" },
+    { "<leader>fO", function () require("oil").open(vim.uv.cwd()) end, desc = "Open Oil explorer (cwd)" },
+  },
+  opts = {
+    default_file_explorer = false,
+    columns = {
+      "icon",
+      "permissions",
+      "size",
+      "mtime",
+    },
+    view_options = {
+      show_hidden = true,
+    },
+  },
+}
